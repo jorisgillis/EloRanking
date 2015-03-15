7890539a5cf7ab861031cf9d3d3b85536c275ce4
Joris Gillis <joris.gillis@telenet.be>
2015-03-15 13:17:54 +0100

    Initial Commit
    Adding teams works

---
+++ server.R
@@ -0,0 +1,50 @@
+
+library(shiny)
+library(shinydashboard)
+library(dplyr)
+
+##-----------------------------------------------------------------------------
+## LOADING DATA
+## Loads the \code{elo.RData} file which contains:
+## - teams: data frame containing each team with its ELO ranking
+##-----------------------------------------------------------------------------
+load('elo.RData')
+
+
+shinyServer(function(input, output, session) {
+  ##---------------------------------------------------------------------------
+  ## ACCESSING DATA
+  ##---------------------------------------------------------------------------
+  values <- reactiveValues(teams = teams)
+  
+  ##---------------------------------------------------------------------------
+  ## CUD: Create, Update, Delete
+  ##---------------------------------------------------------------------------
+  observeEvent(input$teamNameEnter, 
+               function() {
+                 if (!(input$teamName %in% teams$Team)) {
+                   # Team name is new; add it to the data frame
+                   teams <- rbind(teams, c(input$teamName, 1500))
+                   save(teams, file = 'elo.RData')
+                   
+                   # Update all teams-dependent shows
+                   values$teams <- teams
+                   
+                   # Clear the input field
+                   updateTextInput(session, inputId = 'teamName', value = '')
+                 }
+               })
+  
+  ##---------------------------------------------------------------------------
+  ## SHOWING DATA
+  ##---------------------------------------------------------------------------
+  output$teamsTable <- renderDataTable({
+    values$teams
+  }, 
+  options = list(
+    pageLength= 24,
+    lengthMenu = c(24),
+    dom = 't'
+  ))
+  
+})
---
+++ ui.R
@@ -0,0 +1,39 @@
+library(shinydashboard)
+
+shinyUI(dashboardPage(
+  dashboardHeader(title = 'ELO Soccer'),
+  dashboardSidebar(
+    sidebarMenu(
+      menuItem('Data Entry', tabName = 'dataEntry', icon = icon('keyboard-o')),
+      menuItem('ELO Ratings', tabName = 'eloRatings', icon = icon('bar-chart-o'))
+    )
+  ),
+  dashboardBody(
+    tabItems(
+      tabItem(tabName = 'dataEntry',
+              h4('Data Entry'),
+              fluidRow(
+                box(width = 12, solidHeader = T, status = 'primary', title = 'Manage Teams', 
+                    fluidRow(
+                      column(8, textInput('teamName', "Team name:")), 
+                      column(4, actionButton('teamNameEnter', label = 'Add', icon = icon('check')))
+                    )
+                )
+              ),
+              fluidRow(
+                box(width = 12, status = 'success', title = 'Teams',
+                    fluidRow(
+                      column(12, dataTableOutput('teamsTable'))
+                    )
+                )
+              ),
+              fluidRow(
+                box(width = 12, solidHeader = T, status = 'primary', title = 'Enter players', p("Players"))
+              )
+      ),
+      tabItem(tabName = 'eloRatings',
+              h4('ELO Ratings')
+      )
+    )
+  )
+))
